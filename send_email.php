0677fd6e1579624ed8e30e8b841845e9f48083ac
ZxJeisonxZ <jeissong2002@gmail.com>
2023-09-30 14:09:05 -0500

    intento form php 1

---
+++ send_email.php
@@ -0,0 +1,56 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");  
+header("Access-Control-Allow-Headers: *"); 
+header("Content-Type: application/json");  
+
+
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    echo json_encode(["error" => "Invalid request method"]);
+    exit;
+}
+
+
+$name = isset($_POST["nombre"]) ? trim($_POST["nombre"]) : "";
+$email = isset($_POST["correo"]) ? trim($_POST["correo"]) : "";
+$phone = isset($_POST["telefono"]) ? trim($_POST["telefono"]) : "";
+$message = isset($_POST["mensaje"]) ? trim($_POST["mensaje"]) : "";
+
+if (empty($name) || empty($email) || empty($phone) || empty($message)) {
+    echo json_encode(["error" => "Falta rellenar la información de algunos campos"]);
+    exit;
+}
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+try {
+    $mail->isSMTP();
+    $mail->SMTPAuth = true;
+
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+
+    $mail->Username = "cocolabs.ventas@gmail.com";
+    $mail->Password = "wojzkqqqwbyqiibw"; 
+
+    $mail->setFrom($email, $name);
+    // $mail->addAddress("cocolabs.ventas@gmail.com", "Coco Labs");
+    $mail->addAddress("jeissong2002@gmail.com", "Jeison Gómez");
+
+    $mail->Subject = "Mensaje de contacto de $name";
+    $mail->Body = "Nombre: $name\nCorreo: $email\nTeléfono: $phone\n\nMensaje:\n$message";
+
+    $mail->send();
+    echo json_encode(["success" => "Email sent"]);
+} catch (Exception $e) {
+    echo json_encode(["error" => "No se pudo enviar el mensaje. Mailer Error: {$mail->ErrorInfo}"]);
+}
+
+?>
+
