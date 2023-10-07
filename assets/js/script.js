c5e2730058b2aa38194847c1f217564e11502a4e
ZxJeisonxZ <jeissong2002@gmail.com>
2023-10-07 08:35:12 -0500

    blog

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,3 +49,49 @@ window.addEventListener("scroll", function () {
   window.scrollY >= 800 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
 
 });
+
+// Obtener elementos del DOM y almacenarlos en un arreglo
+var openModalBtns = [
+  document.getElementById('openModalBtn'),
+  document.getElementById('openModalBtn2'),
+  document.getElementById('openModalBtn3'),
+  document.getElementById('openModalBtn4')
+];
+
+var closeModalBtns = [
+  document.getElementById('closeModalBtn'),
+  document.getElementById('closeModalBtn2'),
+  document.getElementById('closeModalBtn3'),
+  document.getElementById('closeModalBtn4')
+];
+
+var modals = [
+  document.getElementById('myModal'),
+  document.getElementById('myModal2'),
+  document.getElementById('myModal3'),
+  document.getElementById('myModal4')
+];
+
+// Agregar evento a los botones para abrir las modales correspondientes
+openModalBtns.forEach(function (btn, index) {
+  btn.addEventListener('click', function () {
+      modals[index].style.display = 'block';
+  });
+});
+
+// Agregar evento a los botones para cerrar las modales correspondientes
+closeModalBtns.forEach(function (btn, index) {
+  btn.addEventListener('click', function () {
+      modals[index].style.display = 'none';
+  });
+});
+
+// Cerrar las modales si se hace clic fuera de ellas
+window.addEventListener('click', function (event) {
+  modals.forEach(function (modal) {
+      if (event.target == modal) {
+          modal.style.display = 'none';
+      }
+  });
+});
+
